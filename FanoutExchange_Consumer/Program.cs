35e678d7bba565f0fc2b4414b07a4d1157e76e39
Hakan <hakanyalitekin@gmail.com>
2021-05-15 00:26:25 +0300

    Fanout Exchange kodları eklendi.

--- FanoutExchange_Consumer/Program.cs
+++ FanoutExchange_Consumer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace FanoutExchange_Consumer
 {
@@ -6,7 +10,48 @@ namespace FanoutExchange_Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //Yukarda oluşturduğumuza bağlantı üzerinden bir kanal oluşturuyoruz.
+            IModel channel = connection.CreateModel();
+
+
+            /*RabbitMQ'nun bizim için rastgele bir isimde gecici kuyruk oluşuturmasını sağlıyoruz.
+             Bu bi zorunluluk değil, bu Consumer'ı birde çok kez çalıştırıp aynı mesajları aldığını 
+             göstermek için oluşturdum.*/
+            #region Eğer kalıcı bir kuyruk oluşturmak isteseydik
+            //var randomQueueName = "log-database-save"; //channel.QueueDeclare().QueueName;
+            //channel.QueueDeclare(randomQueueName, true, false, true, null);
+            #endregion
+            var randomQueueName = channel.QueueDeclare().QueueName;
+
+            //Kuyarıda tanımladığımız kuyruğu exchange'imizle eşleştiriyoruz.
+            channel.QueueBind(queue: randomQueueName, exchange: "logs-fanout", routingKey: string.Empty, arguments: null);
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            //Yukarıda oluşturulan randomQueueName'i burada kullanıyoryz.
+            channel.BasicConsume(queue: randomQueueName, autoAck: false, consumer: consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1000);
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                /*Mesajların doğru bir şekilde işlendiğini ve kuyruktan silineceğini bildiriyoruz.
+                multiple -> işlenmiş ama rabbitmq'dan silinmemiş mesajlarıda silsin istersek true demeliyiz.
+                biz sadece kendi mesajımızla ilgileneceğimiz için false diyoruz.*/
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            Console.ReadKey();
         }
     }
 }
--- FanoutExchange_Publisher/Program.cs
+++ FanoutExchange_Publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace FanoutExchange_Publisher
 {
@@ -6,7 +9,34 @@ namespace FanoutExchange_Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            //channel.QueueDeclare(queue: "logs", durable: true, exclusive: false, autoDelete: false);
+
+            channel.ExchangeDeclare(exchange:"logs-fanout", type:ExchangeType.Fanout, durable:true);
+
+            Enumerable.Range(1, 20).ToList().ForEach(x =>
+            {
+                //Mesajımızı oluşuturup, byte array'e çeviriyoruz.
+                string logMessage = $"LogID -> {x}";
+                var logMessageBody = Encoding.UTF8.GetBytes(logMessage);
+
+                /* Fanout tipli exchange'lerimiz mesajlarımız herhangi bir routing key'imiz  
+                 olmadığı için ikinci parametremizi boş bırakıyoruz (string.Empty).Bir önceki 
+                 örneğimizde ilk parametre boş ikinci parametre yani queue logs'tu */
+                channel.BasicPublish("logs-fanout", string.Empty, null, logMessageBody);
+
+                Console.WriteLine($"Log kuyruğa gönderilmiştir: {logMessage}");
+            });
+
+            Console.ReadKey();
         }
     }
 }
