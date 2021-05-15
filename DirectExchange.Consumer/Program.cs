66541656dcd4e01f115aca5d92f8eb4de2b4e13d
Hakan <hakanyalitekin@gmail.com>
2021-05-15 18:47:19 +0300

    Direct Exchange kodları eklendi.

--- DirectExchange.Consumer/Program.cs
+++ DirectExchange.Consumer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace DirectExchange.Consumer
 {
@@ -6,7 +10,40 @@ namespace DirectExchange.Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //Yukarda oluşturduğumuza bağlantı üzerinden bir kanal oluşturuyoruz.
+            IModel channel = connection.CreateModel();
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            //Kullanıcın dinlemek istediği tipi belirlemesini sağlıyoruz.
+            Console.WriteLine("Dinlemek istediğiniz log tipini(Error,Warning,Info) belirtiniz:");
+            var selectedType = Console.ReadLine();
+            var queueName = $"direct-queue-{selectedType}";
+
+            //Yukarıda oluşturulan queueName'i burada kullanıyoryz.
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1000);
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                /*Mesajların doğru bir şekilde işlendiğini ve kuyruktan silineceğini bildiriyoruz.
+                multiple -> işlenmiş ama rabbitmq'dan silinmemiş mesajlarıda silsin istersek true demeliyiz.
+                biz sadece kendi mesajımızla ilgileneceğimiz için false diyoruz.*/
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            Console.ReadKey();
         }
     }
 }
--- DirectExchange.Publisher/Program.cs
+++ DirectExchange.Publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace DirectExchange.Publisher
 {
@@ -6,7 +9,55 @@ namespace DirectExchange.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            //Exchange adımızı ve tipimizi aşağıdaki gibi düzenliyoruz.
+            channel.ExchangeDeclare(exchange: "logs-direct", type: ExchangeType.Direct, durable: true);
+
+            /*Bir önceki Fanout örneğinde kuyruk deklare etme işini Consumer tarafına bırakmıştık.
+             Burada ise Publisher tarafında oluşturup, yine bind etme işlemini da burada gerçekleştirdik.
+             En aşağıda -LogTypes- adında bir enum tanımladık ve içerisene 3 tip hata ekledik. 
+             Bu 3 hata arasında dönüp 3 adet kuyruk oluşturup bind işlemini gerçekleştiriyoruz.
+            */
+            Enum.GetNames(typeof(LogTypes)).ToList().ForEach(x =>
+            {
+                var routeKey = $"route-{x}";
+                var queueName = $"direct-queue-{x}";
+
+                //Kuruğumuzu oluşturuyoruz.
+                channel.QueueDeclare(queue: queueName, durable: true, exclusive: false, autoDelete: false);
+
+                //Oluştuduğumuz kuruk ile exchange'imizi routeKey tanımlaması yaparak bind ediyoruz.
+                channel.QueueBind(queue: queueName, exchange: "logs-direct", routeKey, null);
+            });
+
+            Enumerable.Range(1, 20).ToList().ForEach(x =>
+            {
+                /*Senaryo gereği rastgele tiplerten ve mesajlardan oluşan toplam 20 adet mesaj üretiyoruz.*/
+                LogTypes logType = (LogTypes)new Random().Next(1, 4);
+                string logMessage = $"Log Type -> {logType}";
+                var routeKey = $"route-{logType}";
+
+                //Mesajımızı oluşuturup, byte array'e çeviriyoruz.
+                var logMessageBody = Encoding.UTF8.GetBytes(logMessage);
+
+                /*İlk iki örneğimizde RoutingKey'i boş geçiyorduk artık
+                 yukarıda oluşan routeKey'i kullanıyoruz*/
+                channel.BasicPublish("logs-direct", routeKey, null, logMessageBody);
+
+                Console.WriteLine($"Log kuyruğa gönderilmiştir: {logMessage}");
+            });
+
+            Console.ReadKey();
         }
     }
+    //Senaryo gereği oluşturduğumuz enum
+    enum LogTypes { Error = 1, Warning = 2, Info = 3 }
 }
