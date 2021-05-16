989d5b8f7ad767b9b0974e41a044feb70899e729
Hakan <hakanyalitekin@gmail.com>
2021-05-17 00:32:30 +0300

    Topic Exchange kodları eklendi.

--- TopicExchange.Consumer/Program.cs
+++ TopicExchange.Consumer/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+using System.Threading;
 
 namespace TopicExchange.Consumer
 {
@@ -6,7 +10,47 @@ namespace TopicExchange.Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //Yukarda oluşturduğumuza bağlantı üzerinden bir kanal oluşturuyoruz.
+            IModel channel = connection.CreateModel();
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            Console.WriteLine("Route Key'in ortasında Error olan kuyruklar dinleniyor.");
+            //Random isimli bir kuyruk tanımlıyoruz.
+            var queueName = channel.QueueDeclare().QueueName;
+
+            //Başı ve sonu önemli değil ortasında error yazanlar gelsin istiyoruz.
+            var routeKey = "*.Error.*";
+            //var routeKey = "Info.*.Error"; ==> Başı Info sonu Error olanlar.
+            //var routeKey = "#.Info"; ==> Başın ne olduğu önemli değil sonu Info olanlar.
+
+            //Bind işlemini gerçekleştiriyoruz.
+            channel.QueueBind(queue: queueName, exchange: "logs-topic", routingKey: routeKey);
+
+            //Yukarıda oluşturulan queueName'i burada kullanıyoruz.
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1000);
+                Console.WriteLine("Gelen Mesaj: " + message);
+
+                /*Mesajların doğru bir şekilde işlendiğini ve kuyruktan silineceğini bildiriyoruz.
+                multiple -> işlenmiş ama rabbitmq'dan silinmemiş mesajlarıda silsin istersek true demeliyiz.
+                biz sadece kendi mesajımızla ilgileneceğimiz için false diyoruz.*/
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            Console.ReadKey();
         }
     }
 }
--- TopicExchange.Publisher/Program.cs
+++ TopicExchange.Publisher/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Linq;
+using System.Text;
 
 namespace TopicExchange.Publisher
 {
@@ -6,7 +9,42 @@ namespace TopicExchange.Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            IModel channel = connection.CreateModel();
+
+            //Exchange adımızı ve tipimizi aşağıdaki gibi düzenliyoruz.
+            channel.ExchangeDeclare(exchange: "logs-topic", type: ExchangeType.Topic, durable: true);
+
+            Enumerable.Range(1, 20).ToList().ForEach(x =>
+            {
+                /*Topic senaryo gereği 3 çeşit rastgele tiplerten ve mesajlardan oluşan toplam 20 adet mesaj üretiyoruz.*/
+                Random rnm = new Random();
+                LogTypes log1 = (LogTypes)rnm.Next(1, 4);
+                LogTypes log2 = (LogTypes)rnm.Next(1, 4);
+                LogTypes log3 = (LogTypes)rnm.Next(1, 4);
+
+                //Mesajımızı ve RouteKey'imizi Topic senaryosuna göre güncelliyoruz.
+                var routeKey = $"{log1}.{log2}.{log3}";
+                string logMessage = $"Log Types -> {log1}-{log2}-{log3}";
+
+                //Mesajımızı oluşuturup, byte array'e çeviriyoruz.
+                var logMessageBody = Encoding.UTF8.GetBytes(logMessage);
+
+                /*Route Key'i logs-topic olarak güncelliyoruz. */
+                channel.BasicPublish("logs-topic", routeKey, null, logMessageBody);
+
+                Console.WriteLine($"Log kuyruğa gönderilmiştir: {logMessage}");
+            });
+
+            Console.ReadKey();
         }
     }
+    //Senaryo gereği oluşturduğumuz enum
+    enum LogTypes { Error = 1, Warning = 2, Info = 3 }
 }
