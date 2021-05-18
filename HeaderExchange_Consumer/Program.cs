481af7ec15d5e76b2690d668d27f93be331d1350
Hakan <hakanyalitekin@gmail.com>
2021-05-19 02:04:41 +0300

    Header Exchange kodları eklendi.

--- HeaderExchange_Consumer/Program.cs
+++ HeaderExchange_Consumer/Program.cs
@@ -1,4 +1,9 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading;
 
 namespace HeaderExchange_Consumer
 {
@@ -6,7 +11,54 @@ namespace HeaderExchange_Consumer
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //Yukarda oluşturduğumuza bağlantı üzerinden bir kanal oluşturuyoruz.
+            IModel channel = connection.CreateModel();
+
+            //Yeniden declare etmek zorunda değiliz. Önce Consumer'ı ayağa kaldırırsak hata almamak için ekledik
+            channel.ExchangeDeclare(exchange: "logs-header", type: ExchangeType.Headers, durable: true);
+
+
+            channel.BasicQos(prefetchSize: 0, prefetchCount: 1, global: false);
+            var consumer = new EventingBasicConsumer(channel);
+
+            //Random isimli bir kuyruk tanımlıyoruz.
+            var queueName = channel.QueueDeclare().QueueName;
+
+            /* x-match zorunludur.
+                    any→ değerlerden herhangi birinin eşleşmesi şartı
+                    all → tüm değerlerin eşleşmesi şartı */
+            Dictionary<string, object> header = new Dictionary<string, object>();
+            header.Add("logLevel", "Error"); //Biz belirliyoruz
+            header.Add("reportFormat", "pdf"); //Biz belirliyoruz.
+            header.Add("x-match","all"); //Biz BELİRLEMİYORUZ. (any ya da all olarak karar veriyoruz.)
+
+            //header'ımızı da ekleyip bind işlemini gerçekleştiriyoruz.
+            channel.QueueBind(queue: queueName, exchange: "logs-header", routingKey: string.Empty, arguments:header);
+
+            //Yukarıda oluşturulan queueName'i burada kullanıyoruz.
+            channel.BasicConsume(queue: queueName, autoAck: false, consumer: consumer);
+
+            Console.WriteLine("Kuyruk dinleniyor...");
+            consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+            {
+                var message = Encoding.UTF8.GetString(e.Body.ToArray());
+                Thread.Sleep(1000);
+                Console.WriteLine("Mail olarak gönderilen rapor : " + message);
+
+                /*Mesajların doğru bir şekilde işlendiğini ve kuyruktan silineceğini bildiriyoruz.
+                multiple -> işlenmiş ama rabbitmq'dan silinmemiş mesajlarıda silsin istersek true demeliyiz.
+                biz sadece kendi mesajımızla ilgileneceğimiz için false diyoruz.*/
+                channel.BasicAck(deliveryTag: e.DeliveryTag, multiple: false);
+            };
+
+            Console.ReadKey();
         }
     }
 }
--- HeaderExchange_Publisher/Program.cs
+++ HeaderExchange_Publisher/Program.cs
@@ -1,4 +1,8 @@
-﻿using System;
+﻿using RabbitMQ.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 
 namespace HeaderExchange_Publisher
 {
@@ -6,7 +10,38 @@ namespace HeaderExchange_Publisher
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            //Öncelikle RabbitMQ'ya bağlanıyoruz.
+            var factory = new ConnectionFactory();
+            factory.Uri = new Uri("amqp://guest:guest@localhost:5672");
+
+            //Bağlantı açıyoruz
+            using var connection = factory.CreateConnection();
+
+            //Yukarda oluşturduğumuza bağlantı üzerinden bir kanal oluşturuyoruz.
+            IModel channel = connection.CreateModel();
+
+            //Exchange adımızı ve tipimizi aşağıdaki gibi düzenliyoruz.
+            channel.ExchangeDeclare(exchange: "logs-header", type: ExchangeType.Headers, durable: true);
+
+            //Mesajımızı iletirken header de göndermek üzere bir key-value obje oluşturalım.
+            Dictionary<string, object> header = new Dictionary<string, object>();
+            header.Add("logLevel", "Error");
+            header.Add("reportFormat", "pdf");
+
+            //Oluşturulan header'ı, aşağıdaki gibi Headers'a eşitleyelim
+            var properties = channel.CreateBasicProperties();
+            properties.Headers = header;
+
+            //Örnek bir rapor içeriği olduğunu varsayalım.
+            var message = Encoding.UTF8.GetBytes("----Kritik_Hata_Raporu.pdf----");
+
+            /*Exchange tipimizi belirtiyoruz, route key kullanamdığımız için boş geçiyoruz.
+             mesajımızın header'ını doldurduğumuz için yukarıda tanımladığımız propertiyi gönderiyoruz.
+            Son olarakta mesajımızı ekliyoruz. */
+            channel.BasicPublish(exchange:"logs-header",routingKey:string.Empty, basicProperties:properties, body : message);
+
+            Console.WriteLine("Kritik_Hata_Raporu.pdf RabbitMQ'ya gönderilmiştir.");
+            Console.ReadKey();
         }
     }
 }
